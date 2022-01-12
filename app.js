7b0a45ed7700ba949310bb07920b412f55460db9
Eden Castillo Asport <ecastilloasport@gmail.com>
2022-01-12 17:11:03 -0500

    [FIX] COmpletely remove all native date code. Intalled moment.js for better time control. Imporve calculate view

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-// Load the full build.
-var _ = require('lodash');
-const { raw } = require("express");
+var moment = require('moment'); // require
+moment().format(); 
 
-const numberOfDays = 10;
+// Load the full build.
+var _ = require("lodash");
+const { raw } = require("express");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// End Points 
+// End Points
 
-app.get("/", function(req, res) {
-    res.render('home',{});
+app.get("/", function (req, res) {
+  res.render("home", {});
 });
 
 // app.post('/test', (req, res) => {
@@ -33,40 +34,33 @@ app.get("/", function(req, res) {
 //     });
 //   });
 
-app.post("/calculate", function(req, res){
-
-    rawDate = req.body.postDate;
-    const date = new Date();
-    console.log("Today is: "+ date);
-    console.log("Unformated birthDate: "+rawDate);
-    const year = (rawDate).split('-')[0];
-    const month = (rawDate).split('-')[1]-1;
-    const day = (rawDate).split('-')[2];
-    const birthDate = new Date(year, month, day);
-    console.log("Birthdate: "+ birthDate);
-    const tenDays = new Date();
-    const hundredDays = new Date();
-    const thousandDays = new Date();
-
-    tenDays.setDate(birthDate.getDate() + numberOfDays);
-    hundredDays.setDate(birthDate.getDate() + Math.pow(numberOfDays, 2));
-    thousandDays.setDate(birthDate.getDate() + Math.pow(numberOfDays, 3));
-
-    console.log("After 10 days: " + tenDays);
-    console.log("After 100 days: " + hundredDays);
-    console.log("After 1000 days: " + thousandDays);
-
+app.post("/calculate", function (req, res) {
+    let message = "is";
+    const now = moment();
+    console.log("POST: " + req.body.userBDate);
+    const userBDate = req.body.userBDate;
+    const userBirthDate = moment(userBDate);
+    const userBirthDateThisYear = moment(userBirthDate).set('year', moment(now).year());
+    console.log(userBDate);
+    console.log(`userBirthDate: ${userBirthDate}`);
+    console.log(`now: ${now}`);
+    if( moment(userBirthDateThisYear).isBefore(now) ){
+        console.log(`isBEfore`);
+        message = "was";
+    }
+    // Addition of days to user inpput birthdate
     const mathBirthDays = {
-        tenDays: tenDays.toDateString(),
-        hundredDays: hundredDays.toDateString(),
-        thousandDays: thousandDays.toDateString()
-    };
-    
-
+        tenDays: userBirthDate.add(10,'d').format("YYYY MMMM DD"),
+        hundredDays: userBirthDate.add(100,'d').format("YYYY MMMM DD"),
+        thousandDays: userBirthDate.add(1000,'d').format("YYYY MMMM DD")
+    }
     console.log(mathBirthDays);
-    res.render("calculate", { mathBirthDays: mathBirthDays});
+    res.render("calculate", { 
+        mathBirthDays: mathBirthDays,
+        message: message
+     });
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Server is running on port  3000.");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port  3000.");
 });
